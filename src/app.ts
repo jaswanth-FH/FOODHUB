afad28f1750708f744de4e15158bb3f98a3a0935
JPXL <jaswanth.p@foodhub.com>
2026-01-14 06:14:36 +0000

    feat(cors) : added cors for frontend access.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import bootstrapRoute from "./routes/bootstrap.route";
 import { errorHandler } from "./utils/errorHandler";
+
+import cors  from "cors";
 import clientsRoute from "./routes/clients.route";
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/", bootstrapRoute);
